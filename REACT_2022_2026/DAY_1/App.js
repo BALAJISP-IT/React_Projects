b27144a7caaacdb608f09a6cdc69d0a9ca02319d
BALAJISP-IT <124854660+BALAJISP-IT@users.noreply.github.com>
2023-11-24 11:25:36 +0530

    Add files via upload

---
+++ REACT_2022_2026/DAY_1/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import HelloWorld from './classComponents/democlass';
+
+import Hello from './functionalComponent/Hello';
+function App() {
+  return (
+    <div className="App" >
+     
+    
+     <Hello/> 
+    <HelloWorld/>
+     
+    </div>
+  );
+}
+
+export default App;
